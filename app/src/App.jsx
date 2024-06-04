ade27d79499ebeb2191597101f6f21220dae8f99
Adrian <adriangonzaloma@gmail.com>
2024-06-04 11:37:47 +0200

    create header component; add style the component #5

---
+++ app/src/App.jsx
@@ -0,0 +1,19 @@
+// import React, { Component } from 'react';
+import React from "react"
+
+import Header from './component/Header.jsx'
+
+function App(){
+    return(<Header />)
+}
+
+export default App
+
+// class App extends Component{
+//     constructor(){
+//         super()
+//     }
+//     render(){
+//         return(<Header />)
+//     }
+// }
