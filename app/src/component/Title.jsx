5b0e3d0ef560d6ccf77588d2fed0e75f91c0e3f9
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-06-08 17:16:58 +0200

    add new function task Completed #17

---
+++ app/src/component/Title.jsx
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+import "./Title.css"
+
+function Title() {
+	return (
+		<div className="Title">
+			<h1>TaskFlow</h1>
+		</div>
+	)
+}
+
+export default Title
